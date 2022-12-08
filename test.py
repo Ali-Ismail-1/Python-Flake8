e53bd5ede207446d6a2e138ebec36a33fc48b467
Ali-Ismail <ali@cryptofi.tech>
2022-12-08 11:02:10 -0800

    testing white spaces

--- test.py
+++ test.py
@@ -1,17 +1,10 @@
 ### Hello world ###
-
-
-
-
 def helloWorld():
     print('hello world')
-    
-    
-    
 class test():
     def test_method():
         print('this is a test method')
-        
+
     def another_test_method():
         print('this is another test method')
-        
+

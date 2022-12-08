6c8eaa660f0c285470ba38bd3d9cb1d452e85ad9
Ali-Ismail <ali@cryptofi.tech>
2022-12-08 10:41:13 -0800

    created a good main, testing if corrections worked

---
+++ main-good.py
@@ -0,0 +1,18 @@
+def main():
+    print('main')
+
+
+def tooCLose():
+    print('too close')
+
+
+def quotes():
+    print("QUOTES")
+    
+    
+def badSpaces():
+    print('bad spacing')
+  
+  
+def tooLong():
+    print('12345678901234567890')

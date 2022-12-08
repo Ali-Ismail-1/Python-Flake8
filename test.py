d6955038975cc93a98d607a194da03eba4350d7f
Ali-Ismail <ali@cryptofi.tech>
2022-12-08 11:03:55 -0800

    used pyCharm to fix PEP8 errors

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
-### Hello world ###
+"""
+This is a block comment
+"""
 def helloWorld():
     print('hello world')
+
+
 class test():
-    def test_method():
+    def test_method(self):
         print('this is a test method')
 
-    def another_test_method():
+    def another_test_method(self):
         print('this is another test method')
-
